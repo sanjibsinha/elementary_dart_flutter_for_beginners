0182315b61092d9ad5c03c7b486016ff3f5dcd22
Sanjib Sinha <sanjib12sinha@gmail.com>
2022-01-12 08:55:17 +0530

    Second to first chapter

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,35 @@
 //import 'package:flutter/material.dart';
+class Student {
+  final String _name = 'A Student';
+  String get name => _name;
+  late final School school;
+}
 
-void main() {
-  /// this is chapter one
-  ///
-  int? x = 1;
+class School {
+  final String _name = 'L\'cole';
+  String get name => _name;
+  late final Student student;
+}
+
+void main(List<String> args) {
+  int? x;
+  x = null;
+  // output is null
   print(x);
-  //runApp(const MyApp());
+  List<String?> theListThatCanHoldNullValue = ['first', 'second', null];
+  print(theListThatCanHoldNullValue.length); // 3
+  print(theListThatCanHoldNullValue[2]);
+  //int? a = null;
+  //int b = a;
+  // a value of type int? cannot ne assigned to a variable of 'type' int
+  //print(b);
+  String? _aTextThatCouldBeNullButNot() => 'Not Null';
+  String? notNull = _aTextThatCouldBeNullButNot();
+  print(notNull);
+  final aStudent = Student();
+  final aSchool = School();
+  aStudent.school = aSchool;
+  aSchool.student = aStudent;
+  print('A student gets a school ' + aStudent.school.name);
+  print('A school gets a student ' + aSchool.student.name);
 }
