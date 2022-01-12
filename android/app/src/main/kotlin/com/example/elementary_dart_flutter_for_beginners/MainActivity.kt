09da183bd33213fc935269ae6fd1eda8483212be
Sanjib Sinha <sanjib12sinha@gmail.com>
2022-01-12 07:17:32 +0530

    testing

---
+++ android/app/src/main/kotlin/com/example/elementary_dart_flutter_for_beginners/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.elementary_dart_flutter_for_beginners
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+//import 'package:flutter/material.dart';
+
+void main() {
+  int? x = 1;
+  print(x);
+  //runApp(const MyApp());
+}
