9b978c95e8d4a159dd49da3cecc6f184887a01e6
Sanjib Sinha <sanjib12sinha@gmail.com>
2022-01-12 12:36:48 +0530

    Third to first chapter

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,53 @@
-//import 'package:flutter/material.dart';
-class Student {
-  final String _name = 'A Student';
-  String get name => _name;
-  late final School school;
-}
-
-class School {
-  final String _name = 'L\'cole';
-  String get name => _name;
-  late final Student student;
-}
+import 'package:flutter/material.dart';
 
 void main(List<String> args) {
-  int? x;
-  x = null;
-  // output is null
-  print(x);
-  List<String?> theListThatCanHoldNullValue = ['first', 'second', null];
-  print(theListThatCanHoldNullValue.length); // 3
-  print(theListThatCanHoldNullValue[2]);
-  //int? a = null;
-  //int b = a;
-  // a value of type int? cannot ne assigned to a variable of 'type' int
-  //print(b);
-  String? _aTextThatCouldBeNullButNot() => 'Not Null';
-  String? notNull = _aTextThatCouldBeNullButNot();
-  print(notNull);
-  final aStudent = Student();
-  final aSchool = School();
-  aStudent.school = aSchool;
-  aSchool.student = aStudent;
-  print('A student gets a school ' + aStudent.school.name);
-  print('A school gets a student ' + aSchool.student.name);
+  runApp(const MyApp(
+    title: null,
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key, required this.title}) : super(key: key);
+
+  final String? title;
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'It can\'t be NULL',
+      home: MyHomePage(title: null),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({
+    Key? key,
+    required this.title,
+  }) : super(key: key);
+
+  final String? title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          title != null ? 'It\'s not NULL value' : 'It\'s a NULL value',
+        ),
+      ),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(20),
+          child: Text(
+            title != null ? 'It\'s not NULL value' : 'It\'s a NULL value',
+            style: const TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 }
