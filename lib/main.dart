6bc6b1919b0481afabc6755026fa40b907c7c4f4
Sanjib Sinha <sanjib12sinha@gmail.com>
2022-01-18 09:06:56 +0530

    first push final

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,35 @@
 /// here is a simple demonstration for beginners
 
 void main() {
+  bool? toBe = true;
+  bool? notToBe = false;
+
+  /// isExisting becomes false
+  bool? isExisting = toBe && notToBe;
+
+  if (isExisting) {
+    /// since this part only works if this condition is true
+    /// this part of code won't be executed
+    print('I exist.');
+  } else {
+    /// this will be executed
+    print('I don\'t exist.');
+  }
+
+  /// doIExist becomes true
+
+  bool? doIExist = toBe || notToBe;
+  if (doIExist) {
+    /// since this part only works if this condition is true
+    /// this part of code will be executed
+    print('I exist.');
+  } else {
+    print('I don\'t exist.');
+  }
+
+  /*
   bool good = true;
   bool bad = false;
-  /*
   they are considered to be dead code
    if(good || bad) {
 
