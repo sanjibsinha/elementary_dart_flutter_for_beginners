361d48000c7c198c6deef2f46fda7710fe7a6d4f
Sanjib Sinha <sanjib12sinha@gmail.com>
2022-02-09 05:49:33 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ main() {
   /// everything in Dart is Object
   /// even functions are objects and have a type, Function
   ///
-  bool isTrue() {
+bool isTrue() {
   return true;
 }
 
@@ -14,45 +14,20 @@ bool isFalse() {
 
 var testFalse = isFalse();
 
-int addTwoNumbers(int x, int y) {
+int addNumbers(int x, int y) {
   return x + y;
 }
+print(addNumbers(50, 60));
 
 int addTwoNumbers(int x, int y) => x + y;
 print(addTwoNumbers(10, 20)); // 30
 
 void returnVoid() => print('Returning void');
 
-  isTrue() ? print('It is $testTrue') : print('It is $testFalse');
-  isFalse() ? print('It is $testFalse') : print('It is $testTrue');
-  var result = addTwoNumbers(10, 20);
-  print(result);
-  returnVoid();
-  bool isTrue() {
-  return true;
+isTrue() ? print('It is $testTrue') : print('It is $testFalse');
+isFalse() ? print('It is $testFalse') : print('It is $testTrue');
+var result = addTwoNumbers(10, 20);
+print(result);
+returnVoid();
+  
 }
-
-var testTrue = isTrue();
-
-bool isFalse() {
-  return false;
-}
-
-var testFalse = isFalse();
-
-int addTwoNumbers(int x, int y) {
-  return x + y;
-}
-
-int addTwoNumbers(int x, int y) => x + y;
-print(addTwoNumbers(10, 20)); // 30
-
-void returnVoid() => print('Returning void');
-}
-
-/**
- It is true
-It is true
-30
-  Returning void
- */
