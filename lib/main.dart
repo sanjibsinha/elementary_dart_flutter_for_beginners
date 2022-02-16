c6edccfc41009623b09e644de9bca025519cf5a4
Sanjib Sinha <sanjib12sinha@gmail.com>
2022-02-16 06:19:18 +0530

    final

--- lib/main.dart
+++ lib/main.dart
@@ -1,57 +1,38 @@
-class Context {
-  String? context;
-  Context({this.context});
-}
-
-class StateLess {
-  String? state;
-  Context? context;
-  StateLess({this.state, this.context});
-  void build(Context context) {
-    context = context;
-    print('I am building the ${context.runtimeType} Type');
-  }
-}
-
-class Text extends StateLess {
-  String? text;
-  Text(this.text);
-
-  @override
-  void build(Context context) {
-    print('I am overriding the build method with ${text.runtimeType} Type');
-  }
-}
+import 'package:flutter/material.dart';
 
 main() {
-  Context conextInstance = Context(context: 'I am Context passed as a named parameter.');
-  StateLess stateInstance = StateLess(
-    state: 'I am Stateless',
-    context: conextInstance,
-  );
-  Text textInstance = Text('I am Text');
-  textInstance.state = 'I am representative of State';
-  textInstance.context = Context(context: 'I am Context for Text');
-  textInstance.context!.context = 'I am Context';
-  print(conextInstance.context);
-  print(stateInstance.state);
-  print(textInstance.text);
-
-  print('${stateInstance.context!.context} and ${stateInstance.state}');
-  print('${textInstance.text} and ${textInstance.state} '
-      'and ${textInstance.context!.context}');
-  stateInstance.build(conextInstance);
-  textInstance.build(conextInstance);
+  runApp(const App());
 }
-/**
-// output
 
-I am Context passed as a named parameter.
-I am Stateless
-I am Text
-I am Context passed as a named parameter. and I am Stateless
-I am Text and I am representative of State and I am Context
-I am building the Context Type
-I am overriding the build method with String Type
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
 
-*/
+  static const _title = 'Class Variables and Methods';
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: _title,
+      home: AppHomePage(),
+    );
+  }
+}
+
+class AppHomePage extends StatelessWidget {
+  const AppHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(App._title),
+      ),
+      body: Center(
+        child: Text(
+          App._title,
+          style: Theme.of(context).textTheme.headline4,
+        ),
+      ),
+    );
+  }
+}
