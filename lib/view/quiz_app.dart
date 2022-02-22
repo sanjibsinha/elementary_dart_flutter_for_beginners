98f1fce070ec96f0d2064a878ef5428c3b3ac8fc
Sanjib Sinha <sanjib12sinha@gmail.com>
2022-02-22 09:29:32 +0530

    final

---
+++ lib/view/quiz_app.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'home_page.dart';
+
+class QuizApp extends StatelessWidget {
+  const QuizApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Quiz App Home Page'),
+    );
+  }
+}
