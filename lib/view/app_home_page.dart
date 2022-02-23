a9e14f485d751281e51f0b4e960666c8c76f344f
Sanjib Sinha <sanjib12sinha@gmail.com>
2022-02-23 13:37:34 +0530

    for anonymous

--- lib/view/app_home_page.dart
+++ lib/view/app_home_page.dart
@@ -6,8 +6,9 @@ class AppHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white70,
       appBar: AppBar(
-        title: const Text('Expanded Example'),
+        title: const Text('Anonymous Function Example'),
       ),
       body: Center(
         child: Row(
@@ -15,19 +16,34 @@ class AppHomePage extends StatelessWidget {
             Expanded(
               child: Padding(
                 padding: const EdgeInsets.all(18.0),
-                child: Image.asset('images/lion-king.jpg'),
+                child: GestureDetector(
+                  onTap: () {
+                    print('I am Greedy 3D Buddy.');
+                  },
+                  child: Image.asset('images/greedy.jpg'),
+                ),
               ),
             ),
             Expanded(
               child: Padding(
                 padding: const EdgeInsets.all(18.0),
-                child: Image.asset('images/lion-king.jpg'),
+                child: GestureDetector(
+                  onTap: () {
+                    print('I am Confident 3D Buddy');
+                  },
+                  child: Image.asset('images/confident.jpg'),
+                ),
               ),
             ),
             Expanded(
               child: Padding(
                 padding: const EdgeInsets.all(18.0),
-                child: Image.asset('images/lion-king.jpg'),
+                child: GestureDetector(
+                  onTap: () {
+                    print('I am Careless 3D Buddy.');
+                  },
+                  child: Image.asset('images/careless.jpg'),
+                ),
               ),
             ),
           ],
