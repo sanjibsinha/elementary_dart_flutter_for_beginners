439b034e436a1d57956adaeefab9e96103adcfa2
Sanjib Sinha <sanjib12sinha@gmail.com>
2022-02-28 21:58:41 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,3 +5,19 @@ import 'view/app.dart';
 main() {
   runApp(const App());
 }
+
+/**
+
+void main() {
+  
+print(greet(greeting: 'Hello', name: 'Json Web'));   
+  
+}
+
+String greet({required String greeting, required String name}) {
+  return greeting + ' ' + name;
+}
+
+
+
+*/
