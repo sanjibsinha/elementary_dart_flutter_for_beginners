01c464c1c8bb6f2d73ea1d8714779e9a54f819c5
Sanjib Sinha <sanjib12sinha@gmail.com>
2022-02-28 07:16:49 +0530

    final

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import 'model/mood.dart';
 import 'view/app.dart';
 
-main() {
-  runApp(const App());
+void main() {
+  runApp(
+    /// Providers are above [MyApp] instead of inside it, so that tests
+    /// can use [MyApp] while mocking the providers
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => Mood()),
+      ],
+      child: const App(),
+    ),
+  );
 }
