a8d2b91cd415967641b66825edf8928d1f6dd5d3
Sanjib Sinha <sanjib12sinha@gmail.com>
2022-03-01 06:36:03 +0530

    mood swing completed

--- lib/view/app_home_page.dart
+++ lib/view/app_home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 
 import '../model/mood.dart';
@@ -9,6 +10,7 @@ class AppHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final mood = Provider.of<Mood>(context);
+    final color = Provider.of<Mood>(context);
     return Scaffold(
       backgroundColor: Colors.white70,
       appBar: AppBar(
@@ -23,9 +25,25 @@ class AppHomePage extends StatelessWidget {
                 child: GestureDetector(
                   onTap: () {
                     mood.changeMood();
+                    color.changeColor();
                   },
-                  child:
+                  child: Column(
+                    children: [
                       Image.asset('images/face${mood.leftFaceIndex + 1}.jpg'),
+                      Container(
+                        padding: const EdgeInsets.all(5.0),
+                        child: Text(
+                          'Mood Changes with Color',
+                          style: GoogleFonts.laila(
+                            textStyle: Theme.of(context).textTheme.headline6,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w700,
+                            color: color.colors[color.leftIndex],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
@@ -35,9 +53,25 @@ class AppHomePage extends StatelessWidget {
                 child: GestureDetector(
                   onTap: () {
                     mood.changeMood();
+                    color.changeColor();
                   },
-                  child:
+                  child: Column(
+                    children: [
                       Image.asset('images/face${mood.rightFaceIndex + 1}.jpg'),
+                      Container(
+                        padding: const EdgeInsets.all(5.0),
+                        child: Text(
+                          'Mood Changes with Color',
+                          style: GoogleFonts.laila(
+                            textStyle: Theme.of(context).textTheme.headline6,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w700,
+                            color: color.colors[color.rightIndex],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
