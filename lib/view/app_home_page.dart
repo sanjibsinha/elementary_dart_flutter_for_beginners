ffdee72ce78ca13df2bb3bdc1649ba377d30a928
Sanjib Sinha <sanjib12sinha@gmail.com>
2022-03-04 07:07:19 +0530

    Update app_home_page.dart

--- lib/view/app_home_page.dart
+++ lib/view/app_home_page.dart
@@ -1,60 +1,116 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:provider/provider.dart';
 
-import '../model/mood.dart';
-
-class AppHomePage extends StatelessWidget {
+class AppHomePage extends StatefulWidget {
   const AppHomePage({Key? key}) : super(key: key);
 
   @override
+  _AppHomePageState createState() => _AppHomePageState();
+}
+
+class _AppHomePageState extends State<AppHomePage> {
+  List<String> questions = [
+    '640 acres equal 1 square mile.',
+    'A young horse is called a duckling.',
+    'The sound a Frog makes is known as croak.',
+    'The synonym of scorn is despise.',
+  ];
+
+  int questionIndex = 0;
+
+  List<bool> answers = [
+    true,
+    false,
+    true,
+    true,
+  ];
+
+  String check = '';
+
+  void checkAnswer() {
+    if (questionIndex == 0) {
+      check = '';
+    } else if (questionIndex == 1) {
+      check = 'Corerct Answer was: 640 acres = 1 square mile.';
+    } else if (questionIndex == 2) {
+      check = 'Corerct Answer was: A young horse = a duckling.';
+    } else if (questionIndex == 3) {
+      check = 'Corerct Answer was: The sound a Frog makes = croak.';
+    } else if (questionIndex > 3) {
+      check = 'Corerct Answer was: The synonym of scorn = despise.';
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final mood = Provider.of<Mood>(context);
-    final color = Provider.of<Mood>(context);
     return Scaffold(
-      backgroundColor: Colors.white70,
+      backgroundColor: Colors.blue.shade900,
       appBar: AppBar(
-        title: const Text('Mood Swing App'),
+        title: const Text('Introduction to List'),
       ),
-      body: Center(
-        child: Row(
-          children: [
-            expandMethod(mood, color, context),
-            expandMethod(mood, color, context),
-          ],
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                questions[questionIndex],
+                textAlign: TextAlign.center,
+                style: GoogleFonts.laila(
+                  textStyle: Theme.of(context).textTheme.headline6,
+                  fontSize: 30,
+                  fontWeight: FontWeight.w900,
+                  color: Colors.amber.shade400,
+                ),
+              ),
+              buildButtonInsideContainer(
+                'True',
+                Colors.white,
+              ),
+              buildButtonInsideContainer(
+                'False',
+                Colors.black,
+              ),
+              Text(
+                check,
+                textAlign: TextAlign.center,
+                style: GoogleFonts.caveat(
+                  textStyle: Theme.of(context).textTheme.headline6,
+                  fontSize: 20,
+                  fontWeight: FontWeight.w700,
+                  color: Colors.blue.shade50,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 
-  /// A common function that returns data type Expanded
-  /// later we can use that function
-
-  Expanded expandMethod(Mood mood, Mood color, BuildContext context) {
-    return Expanded(
-      child: Padding(
-        padding: const EdgeInsets.all(18.0),
-        child: GestureDetector(
-          onTap: () {
-            mood.changeMood();
-            color.changeColor();
-          },
-          child: Column(
-            children: [
-              Image.asset('images/face${mood.rightFaceIndex + 1}.jpg'),
-              Container(
-                padding: const EdgeInsets.all(5.0),
-                child: Text(
-                  'Mood Changes with Color',
-                  style: GoogleFonts.laila(
-                    textStyle: Theme.of(context).textTheme.headline6,
-                    fontSize: 20,
-                    fontWeight: FontWeight.w700,
-                    color: color.colors[color.rightIndex],
-                  ),
-                ),
-              ),
-            ],
+  Container buildButtonInsideContainer(String answer, Color color) {
+    return Container(
+      padding: const EdgeInsets.all(10.0),
+      child: ElevatedButton(
+        onPressed: () {
+          setState(() {
+            answers[questionIndex];
+            questionIndex++;
+            checkAnswer();
+          });
+          if (questionIndex > 3) {
+            questionIndex = 0;
+          }
+        },
+        child: Text(
+          answer,
+          style: GoogleFonts.lato(
+            textStyle: Theme.of(context).textTheme.headline6,
+            fontSize: 20,
+            fontWeight: FontWeight.w700,
+            color: color,
           ),
         ),
       ),
